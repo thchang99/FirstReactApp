929ef0a873cfb761ecf70ff6f0d29ba3b20eeffd
thchang99 <thchang99@gmail.com>
2024-11-12 03:10:35 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://i.etsystatic.com/27832863/r/il/c704ae/4020972001/il_570xN.4020972001_k4dl.jpg' className="App-logo" alt="logo" />
+        <p style = {{background : 'blue'}}>
+          22300646 장태희
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
